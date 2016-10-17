bfeb43b6dc39335d10658d11e369c2288d39f87c
VFourny <fourny.valentin@gmail.com>
2016-10-17 10:19:54 +0200

    syntax.c

--- src/syntax.c
+++ src/syntax.c
@@ -11,17 +11,25 @@ node* createNode(typeJeton jet, struct node* nl, struct node* nr){
     return newNode;
 }
 
+//Vérifie les valeurs du tableau jetons et crée l'arbre en cosnséquence
 node* syntax(typeJeton tab[], int i) {
     
     switch (tab[i].lexem) {
-        case OPERATOR:
-            j = i;
-            tmp = syntax(tab, i++);
-            node * newNode = createNode(tab[j], node, tmp);
-            break;
-        case REEL:
+        case FONCTION:
 
             break;
+        case OPERATOR:
+            j = i;
+            //tmp stock le père
+            tmp = syntax(tab, i++);
+            node* newNode = createNode(tab[j], node, tmp);
+            break;
+        case REEL:
+            node* newNode = (tab[i], NULL, NULL);
+            i++;
+            break;
+        case VARIABLE:
+            node* newNode(tab[i], NULL, NULL);
         default:
 
     }
